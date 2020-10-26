d6ddb7d3195bc9ca997b5502da68d79c3f9a17e1
agsedova <anastasiia.sedova@gmail.com>
2020-10-27 00:58:24 +0100

    remove unnecessary files

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ if __name__ == "__main__":
     PatternSearch().retrieve_patterns()
 
 
+'''
+def main(path_to_abstract):
+    with open(path_to_abstract + "_tst_annotated.json", 'w+', encoding="UTF-8") as output_file:
+        annotated_data = []
+        with open(path_to_abstract, 'r') as input_file:
+            wiki_pages = json.loads(input_file.read())
+            for page in wiki_pages:
+                if "may refer to" not in page["text"]:
+                    # page["text"] = page["text"][page["text"].find("\n\n") + 2:]
+
+                    annotated_page = parse_abstract.main(page)
+                    page["annotatedSentences"] = annotated_page
+                    annotated_data.append(page)
+        json.dump(annotated_data, output_file)
+
+
+if __name__ == '__main__':
+    path_to_abstract = sys.argv[1]
+    main(path_to_abstract)
+'''
+
